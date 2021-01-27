5f71ee927d02a79dfdc7c45d701c215808c1f3b8
egg82 <eggys82@gmail.com>
2021-01-26 23:21:15 -0700

    JDA, Kitteh

--- JDA/src/main/java/ninja/egg82/events/JDAEvents.java
+++ JDA/src/main/java/ninja/egg82/events/JDAEvents.java
@@ -7,10 +7,36 @@ import org.jetbrains.annotations.NotNull;
 public class JDAEvents {
     private JDAEvents() { }
 
+    /**
+     * Returns a single event subscriber.
+     *
+     * @param jda the JDA instance to listen to events with
+     * @param event the event class to listen to
+     * @return a new {@link EventSubscriber} that listens to the desired event
+     * @throws NullPointerException if {@code jda} or {@code event} is null
+     */
     public static <T extends GenericEvent> @NotNull JDAEventSubscriber<T> subscribe(@NotNull JDA jda, @NotNull Class<T> event) { return new JDAEventSubscriber<>(jda, event); }
 
+    /**
+     * Returns a merged event subscriber.
+     *
+     * @param jda the JDA instance to listen to events with
+     * @param superclass the event class that will be processed in the handler
+     * @return a new {@link MergedEventSubscriber} that listens to the desired events
+     * @throws NullPointerException if {@code jda} or {@code superclass} is null
+     */
     public static <E1 extends GenericEvent, T> @NotNull JDAMergedEventSubscriber<E1, T> merge(@NotNull JDA jda, @NotNull Class<T> superclass) { return new JDAMergedEventSubscriber<>(jda, superclass); }
 
+    /**
+     * Returns a merged event subscriber
+     * that listens to multiple similar events.
+     *
+     * @param jda the JDA instance to listen to events with
+     * @param superclass the event class that will be processed in the handler
+     * @param events the events to listen to
+     * @return a new {@link MergedEventSubscriber} that listens to the desired events
+     * @throws NullPointerException if {@code jda}, {@code superclass}, or {@code events} are null
+     */
     public static <E1 extends T, T extends GenericEvent> @NotNull JDAMergedEventSubscriber<E1, T> merge(@NotNull JDA jda, @NotNull Class<T> superclass, @NotNull Class<E1>... events) {
         JDAMergedEventSubscriber<E1, T> subscriber = new JDAMergedEventSubscriber<>(jda, superclass);
         for (Class<E1> clazz : events) {
--- KittehIRC/src/main/java/ninja/egg82/events/KittehEvents.java
+++ KittehIRC/src/main/java/ninja/egg82/events/KittehEvents.java
@@ -7,10 +7,36 @@ import org.kitteh.irc.client.library.event.helper.ClientEvent;
 public class KittehEvents {
     private KittehEvents() { }
 
+    /**
+     * Returns a single event subscriber.
+     *
+     * @param client the client instance to listen to events with
+     * @param event the event class to listen to
+     * @return a new {@link EventSubscriber} that listens to the desired event
+     * @throws NullPointerException if {@code client} or {@code event} is null
+     */
     public static <T extends ClientEvent> @NotNull KittehEventSubscriber<T> subscribe(@NotNull Client client, @NotNull Class<T> event) { return new KittehEventSubscriber<>(client, event); }
 
+    /**
+     * Returns a merged event subscriber.
+     *
+     * @param client the client instance to listen to events with
+     * @param superclass the event class that will be processed in the handler
+     * @return a new {@link MergedEventSubscriber} that listens to the desired events
+     * @throws NullPointerException if {@code client} or {@code superclass} is null
+     */
     public static <E1 extends ClientEvent, T> KittehMergedEventSubscriber<E1, T> merge(@NotNull Client client, @NotNull Class<T> superclass) { return new KittehMergedEventSubscriber<>(client, superclass); }
 
+    /**
+     * Returns a merged event subscriber
+     * that listens to multiple similar events.
+     *
+     * @param client the client instance to listen to events with
+     * @param superclass the event class that will be processed in the handler
+     * @param events the events to listen to
+     * @return a new {@link MergedEventSubscriber} that listens to the desired events
+     * @throws NullPointerException if {@code client}, {@code superclass}, or {@code events} are null
+     */
     public static <E1 extends T, T extends ClientEvent> @NotNull KittehMergedEventSubscriber<E1, T> merge(@NotNull Client client, @NotNull Class<T> superclass, @NotNull Class<E1>... events) {
         KittehMergedEventSubscriber<E1, T> subscriber = new KittehMergedEventSubscriber<>(client, superclass);
         for (Class<E1> clazz : events) {
